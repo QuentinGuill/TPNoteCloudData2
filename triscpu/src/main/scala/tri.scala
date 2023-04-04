c5d904e28ded48cdf96e383e0991d6a57a00b16c
guillaumeq <guillaumeq@eisti.eu>
2023-04-04 16:54:32 +0200

    test paralellization

--- triscpu/src/main/scala/tri.scala
+++ triscpu/src/main/scala/tri.scala
@@ -1,13 +1,18 @@
 import scala.io.Source
 
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkContext._ 
+
 /* Tri en utilisant le tri comptage */
 object tri_comptage {
   def main(args: Array[String]): Unit = {
-
     val fileContent = Source.fromFile("10Mega.txt").mkString
     val tableauString  = fileContent.split(" ")
     val tableau = tableauString.map(_.toInt)
+    tri_spark(tableau)
+  }  
 
+  def tri(tableau: Array[Int]): Unit = {
     val result: Array[Int] = new Array[Int](tableau.length)
 
     val min: Int = tableau.min
@@ -29,5 +34,34 @@ object tri_comptage {
     }
 
     println(result.mkString(" "))
-  }  
+  }
+
+  def tri_spark(tableau: Array[Int]): Unit = {
+    val result: Array[Int] = new Array[Int](tableau.length)
+
+    val min: Int = tableau.min
+    val max: Int = tableau.max
+
+    val sparkConf = new SparkConf().setMaster("local").setAppName("Spark Tri")
+    val sc = new SparkContext(sparkConf) 
+
+    var count: Array[Int] = new Array[Int](max - min + 1)
+
+    val rdd = sc.parallelize(count)
+
+    rdd.foreach( (el: Int) => count(el - min) += 1 )
+
+    for (i <- 1 to (max-min)) {
+        count(i) += count(i-1)
+    }
+
+
+    for (el <- tableau.reverseIterator) {
+        count(el - min) -= 1
+        result(count(el - min)) = el
+    }
+
+    println(result.mkString(" "))
+  }
 }
+
